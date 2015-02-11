7df4209de573f0c9dc6a724157cdbecf89e9c335
Michael J. Ayers <ayersmj@redhat.com>
2015-02-10 23:21:13 -0500

    Refactoring plexapi.

--- src/plexctrl.py
+++ src/plexctrl.py
@@ -7,7 +7,12 @@ from	urlparse			import	urljoin
 #import	codecs
 import	os, re, requests, signal, shutil, sys, time, traceback
 import	logging, logging.handlers
-from	plexapi		import server.PlexServer
+from	plexapi.server		import	PlexServer
+from	plexapi.myplex		import	MyPlexUser as PlexUser
+import	cookielib
+from	StringIO			import	StringIO
+from	lxml				import	etree	as letree
+from	xml					import	etree	as xetree
 #from	bs4					import	BeautifulSoup	as	bSoup
 #from	collections			import	OrderedDict
 ###--- My Modules ---------------------------------------------------------------------------------------------------------------
@@ -26,6 +31,7 @@ def argparser():
 	gap.add_argument( 		'--program', 		action = 'store', 		dest = "program",	metavar = "[program]", 	default = os.path.basename( re.sub( ".py", "", sys.argv[0] ) ) )
 	gap.add_argument(		'--search', 		action = 'store', 		dest = "search", 	metavar = "[search]" )
 	gap.add_argument( 		'--config', 		action = 'store', 		dest = "config", 	metavar = "[config]", 	default = 'default.cfg' )
+	gap.add_argument( 		'--xml',	 		action = 'store', 		dest = "xmlparser", metavar = "[XML Parser]", 	default = 'lxml' )
 	gap= ap.add_argument_group( 'authentication' )
 	gap.add_argument( '-l',	'--login', 			action = 'store_true', 	default = False )
 	gap.add_argument( 		'--user', 			action = 'store', 		dest = "username", 	metavar = "[username]" )
@@ -35,6 +41,10 @@ def argparser():
 	gap.add_argument( 		'--logfile', 		action = 'store', 		dest = "logfile", 	metavar = "[logfile]" )
 	gap.add_argument(		'--debug', 			action = 'store_true' )
 	gap.add_argument( '-v', '--verbose', 		action = 'count', 		default = 0 )
+	gap= ap.add_argument_group( 'operations' )
+	gap.add_argument(		'--clients', 		action = 'store_true' )
+	gap.add_argument(		'--recent', 		action = 'store_true' )
+	gap.add_argument(		'--myplex', 		action = 'store_true' )
 
 	try:
 		pArgs = ap.parse_args()
@@ -45,7 +55,7 @@ def argparser():
 		
 	initLog( pArgs.loglevel )
 	logger = logging.getLogger( __name__ )
-
+	
 	if success:
 		return pArgs
 	else:
@@ -92,7 +102,7 @@ def ReadConfig():
 		# Set exit status '1' upon failure and exit.
 		sys.exit( 1 )
 
-	config = configparser.ConfigParser()
+	config = ConfigParser.ConfigParser()
 	config.read( cfgFile )
 		
 	if pArgs.verbose > 3:
@@ -124,15 +134,177 @@ def SetServer( pConfig ):
 		# Set exit status '3' upon failure and exit.
 		sys.exit( 3 )
 	else:
-		servercfg['host'] = pConfig.get( server, host )
-		servercfg['port'] = pConfig.get( server, port )
-		servercfg['username'] = pConfig.get( server, username )
-		servercfg['password'] = pConfig.get( server, password )
+		servercfg['server'] = server
+		servercfg['host'] = pConfig.get( server, 'host' )
+		servercfg['port'] = pConfig.get( server, 'port' )
+		servercfg['username'] = pConfig.get( server, 'username' )
+		servercfg['password'] = pConfig.get( server, 'password' )
 		
 	return servercfg
 ###------------------------------------------------------------------------------------------------------------------------------
+def clients( plex ):
+	logger = logging.getLogger( __name__ )
+
+	for client in plex.clients():
+		logger.info( client.name )
+###------------------------------------------------------------------------------------------------------------------------------
+def DictPrint( data, Name = None, tabbed = False ):
+	logger = logging.getLogger( __name__ )
+	if tabbed:
+		prestring = '\t'
+	else:
+		prestring = ''
+	for key in data.keys():
+		if key == "cookiejar":
+			if Name is None:
+				logger.warn( "%sKey: %s\tValue: %s" % ( prestring, key, "[CookieJar]" ) )
+			else:
+				logger.warn( "%s%s['%s']\tValue: %s" % ( prestring, Name, key, "[CookieJar]" ) )
+		elif key == "pagecontent":
+			if Name is None:
+				logger.warn( "%sKey: %s\tValue: %s" % ( prestring, key, "[PageContent]" ) )
+			else:
+				logger.warn( "%s%s['%s']\tValue: %s" % ( prestring, Name, key, "[PageContent]" ) )
+		else:
+			if Name is None:
+				logger.warn( "%sKey: %s\tValue: %s" % ( prestring, key, data[key] ) )
+			else:
+				logger.warn( "%s%s['%s']\tValue: %s" % ( prestring, Name, key, data[key] ) )
+###------------------------------------------------------------------------------------------------------------------------------
+def pageencode( pagedata ):
+	try:
+		pagedata = str( pagedata, errors = 'ignore' )
+	except:
+		pagedata = str( pagedata )
+	try:
+		pagedata = pagedata.encode( 'ascii', 'ignore' )
+	except:
+		pagedata = pagedata
+	return pagedata
+###------------------------------------------------------------------------------------------------------------------------------
+def getURL( url ):
+	logger = logging.getLogger( __name__ )
+	
+	global rSession
+	CookieJar = cookielib.CookieJar()
+	timeout = '30'
+	stream = False
+
+	#url = "http://apollo.ayercraft.net:32400/library/recentlyAdded"
+	url = "http://apollo.ayercraft.net:32400/library/sections/7/recentlyAdded"
+
+	pagecontent = None
+	reqheaders = dict()
+	respheaders = dict()
+
+	reqheaders['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+	reqheaders['accept-language'] = 'en-US,en;q=0.5'
+	reqheaders['accept-encoding'] = 'gzip,deflate'
+	reqheaders['DNT'] = '1'
+	reqheaders['connection'] = 'keep-alive'
+	reqheaders['user-agent'] = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
+	
+	try:
+		response = rSession.get( url, headers = reqheaders, cookies = CookieJar, timeout = float( timeout ) )
+	except requests.exceptions.HTTPError as exc:
+		logger.error( "(HTTP Error): %s\tURL: " % ( exc, url ) )
+	except requests.exceptions.ConnectionError as exc:
+		logger.error( "(Connection Error) %s\tURL: %s" % ( exc, url ) )
+	except requests.exceptions.Timeout as exc:
+		logger.error( "(Request Timeout Error) %s\tURL: %s" % ( exc, url ) )
+	else:
+		isCType = False
+		if response.status_code == requests.codes.ok:
+			if pArgs.verbose > 3:
+				logger.error( "Good Status Code:\t%s" % ( response.status_code ) )
+				DictPrint( response.headers, Name = 'Response Header' )
+			for key in response.headers.keys():
+				if key == "Content-Type":
+					ctype = "Content-Type"
+					isCType = True
+					logger.debug( "Header: %s\tValue: %s" % ( key, response.headers[key].strip() ) )
+				elif key == "content-type":
+					ctype = "content-type"
+					isCType = True
+					logger.debug( "Header: %s\tValue: %s" % ( key, response.headers[key].strip() ) )
+				respheaders[key] = response.headers[key].strip()
+			if isCType:
+				if ( "text/html" in response.headers[ctype].strip() ) or ( "text/xml" in response.headers[ctype].strip() ) or ( "text/javascript" in response.headers[ctype].strip() ):
+					if pArgs.verbose > 3:
+						logger.debug( "Page returned content type(A):\t%s" % response.headers[ctype].strip() )
+				else:
+					if pArgs.verbose > 3:
+						logger.debug( "Page returned content type(B):\t%s" % response.headers[ctype].strip() )
+		else:
+			logger.error( "Error retrieving url: %s" % ( url ) )
+			logger.error( "\tError:\tBad Status Code:\t%s" % ( response.status_code ) )
+	return response
+###------------------------------------------------------------------------------------------------------------------------------
+def recentlyAdded( servercfg ):
+	logger = logging.getLogger( __name__ )
+
+	url = "http://apollo.ayercraft.net:32400/library/sections/7/recentlyAdded"
+	response = getURL( url )
+	
+	if pArgs.xmlparser == "lxml":
+		xmlContent = letree.fromstring( response.text.encode('utf8') )
+		parser = letree.XMLParser( ns_clean = True )
+		tree = letree.parse( StringIO( xmlContent ), parser )
+	elif pArgs.xmlparser == "xml":
+		xmlContent = xetree.ElementTree.fromstring( response.text.encode('utf8') )
+		parser = xetree.XMLParser( ns_clean = True )
+		tree = xetree.parse( StringIO( xmlContent ), parser )
+		
+	for element in xmlContent.iter():
+		if element.tag == "Directory":
+			mptitle = element.get( 'parentTitle' )
+			mstitle = element.get( 'title' )
+			mtype = element.get( 'type' )
+			logger.info("Title: %s\tType: %s\tSubTitle: %s" % ( mptitle, mstitle, mtype ) )
+		if element.tag == "Video":
+			vtype = element.get( 'type' )
+			if vtype == "episode":
+				vptitle = str( element.get( 'grandparentTitle' ) ).encode('utf-8').strip()
+				vseason = str( element.get( 'parentIndex' ) ).encode('utf-8').strip()
+				vepisode = str( element.get( 'index' ) ).encode('utf-8').strip()
+				vetitle = unicode( element.get( 'title' ) ).encode( 'ascii', 'replace' )
+				logger.info("Title: %s\tSeason: %s\tEpisode: %s\tTitle: %s" % ( vptitle, vseason, vepisode, vetitle ) )
+	
+	#for elem in xmlContent:
+	#	logger.info( xmlContent.keys() )
+	
+	#root = etree.fromstring( content )
+	#parser = etree.XMLParser( ns_clean = True )
+	#tree = etree.parse( StringIO( content ), parser )
+	#for element in root.iter():
+	#	if element.tag == "Directory":
+	#		mptitle = element.get( 'parentTitle' )
+	#		mstitle = element.get( 'title' )
+	#		mtype = element.get( 'type' )
+	#		print("Title: %s\tType: %s\tSubTitle: %s" % ( mptitle, mstitle, mtype ) )
+	#	if element.tag == "Video":
+	#		vtype = element.get( 'type' )
+	#		if vtype == "episode":
+	#			vptitle = str( element.get( 'grandparentTitle' ) ).encode('utf-8').strip()
+	#			vseason = str( element.get( 'parentIndex' ) ).encode('utf-8').strip()
+	#			vepisode = str( element.get( 'index' ) ).encode('utf-8').strip()
+	#			vetitle = unicode( element.get( 'title' ) ).encode( 'ascii', 'replace' )
+	#			print("Title: %s\tSeason: %s\tEpisode: %s\tTitle: %s" % ( vptitle, vseason, vepisode, vetitle ) )
+###------------------------------------------------------------------------------------------------------------------------------
+def myPlex( servercfg ):
+	logger = logging.getLogger( __name__ )
+	
+	from plexapi.myplex import MyPlexUser
+	user = MyPlexUser( servercfg['username'], servercfg['password'] )
+	plex = user.getServer( servercfg['server']).connect
+	
+	print( plex.keys() )
+	#for section in plex.library.sections():
+	#	logger.info( "Section: %s" % ( section.title ) )
+###------------------------------------------------------------------------------------------------------------------------------
 def main():
 	global pArgs
+	global rSession
 	logger = logging.getLogger( __name__ )
 	pArgs = argparser()
 	
@@ -146,7 +318,18 @@ def main():
 		logger.debug( "Server:\t%s:%s" % ( servercfg['host'], servercfg['port'] ) )
 		logger.debug( "Username:\t%s" % ( servercfg['username'] ) )
 		logger.debug( "Password:\t%s" % ( servercfg['password'] ) )
-		
+
+	user = PlexUser( servercfg['username'], servercfg['password'] )
+	plex = user.getServer( servercfg['server'] ).connect()
+
+	rSession = requests.session()
+	
+	if pArgs.clients:
+		clients( plex )
+	elif pArgs.recent:
+		content = recentlyAdded( servercfg )
+	elif pArgs.myplex:
+		content = myPlex( servercfg )
 ###------------------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 	main()
